c5bf8de64768415fb55b1bfcc62e6fee0d24653b
Marat Dukhan <maratek@gmail.com>
2023-12-26 17:34:39 -0800

    Migrate tests from nosetests to pytest
    nosetests doesn't work with Python >= 3.10

--- test/test_k1om.py
+++ test/test_k1om.py
@@ -1,8 +1,3 @@
-import unittest
-
-
-class ReadXML(unittest.TestCase):
-    def runTest(self):
-        from opcodes.k1om import read_instruction_set
-        instruction_set = read_instruction_set()
-
+def test_read_instruction_set():
+    from opcodes.k1om import read_instruction_set
+    instruction_set = read_instruction_set()
