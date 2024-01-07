8fae6652f4ec31bf5dd7c2a6100d45de0d97c2f5
Marat Dukhan <maratek@gmail.com>
2024-01-06 22:27:16 -0800

    Add MCOMMIT instruction

--- opcodes/x86.py
+++ opcodes/x86.py
@@ -467,6 +467,7 @@ class ISAExtension:
         - "CLWB"             := The `CLWB` instruction.
         - "CLZERO"           := The `CLZERO` instruction.
         - "CLDEMOTE"         := The `CLDEMOTE` instruction.
+        - "MCOMMIT"          := The `MCOMMIT` instruction.
         - "PREFETCH"         := The `PREFETCH` instruction (3dnow! Prefetch).
         - "PREFETCHW"        := The `PREFETCHW` instruction (3dnow! Prefetch/Intel PRFCHW).
         - "PREFETCHWT1"      := The `PREFETCHWT1` instruction.
